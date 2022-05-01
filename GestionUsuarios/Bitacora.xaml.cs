2d3889e11e5ae912118187f6ffcdacebf532d0d8
Gabriel <gabrielcorena@gmail.com>
2022-04-30 22:37:47 -0600

    Agregadas pantallas
    prototipo de pantallas

---
+++ GestionUsuarios/Bitacora.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SGVentas.GestionUsuarios
+{
+    /// <summary>
+    /// Lógica de interacción para Bitacora.xaml
+    /// </summary>
+    public partial class Bitacora : Window
+    {
+        public Bitacora()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnVolver_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
