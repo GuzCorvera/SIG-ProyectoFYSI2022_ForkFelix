e3b88da92087bedb421dcadc34f7977cc822f714
Gabriel <gabrielcorena@gmail.com>
2022-05-01 10:06:41 -0600

    agregado conexion a bd

---
+++ ControlBDG.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGVentas
+{
+    internal class ControlBDG
+    {
+        SQLiteConnection cn;
+        
+        public ControlBDG()
+        {
+            
+            cn = new SQLiteConnection(@"data source=C:/SGFYSIEX/SGBD.db");   //CONEXION NORMAL
+
+        }
+
+    }
+}
